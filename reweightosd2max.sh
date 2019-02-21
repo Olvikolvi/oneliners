c7c087f8928413383c7e3b20c513f96e025a4f3c
Mikko Lampikoski <mikko.lampikoski@doop.fi>
2019-02-21 15:14:58 +0200

    new file

---
+++ reweightosd2max.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+OSD="osd.10"
+MAXW="3.63869"
+
+if [ -f reweight2max_gained.tmp ]; then
+  echo "all done.. remove reweight2max_gained.tmp file to restart"
+  exit
+fi
+
+STAUS=$(ceph health|grep "misplaced")
+
+if [ "${STAUS}" == "" ]; then
+  CURW=$(ceph osd tree|grep ${OSD}|awk '{print $3}')
+  NEWW=$(echo "${CURW} + 0.2" | bc)
+  if (( $(echo "${NEWW} > ${MAXW}" | bc -l) )); then
+    echo "ceph osd crush reweight ${OSD} ${MAXW}"
+    touch reweight2max_gained.tmp
+  else
+    echo "ceph osd crush reweight ${OSD} ${NEWW}"
+  fi
+fi
+
+
